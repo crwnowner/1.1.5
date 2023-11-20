75d90cfcfc2dd8b5c8bce78f3f5ea6e739547eaf
crwnowner <8maxabb1122@mail.ru>
2023-11-20 19:10:47 +0300

    Completed 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.saveUser("Игорь", "Игорев", (byte) 23);
+        userServiceImpl.saveUser("Максим", "Максимов", (byte) 24);
+        userServiceImpl.saveUser("Олег", "Олегов", (byte) 25);
+        userServiceImpl.saveUser("Ибрагим", "Ибрагимов", (byte) 26);
+        for (User user : userServiceImpl.getAllUsers()) {
+            System.out.println(user.toString());
+        }
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
