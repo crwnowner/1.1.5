9e285611f3660448018dcdc126d33343914480cd
crwnowner <8maxabb1122@mail.ru>
2023-11-22 11:37:02 +0300

    Completed 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
         userService.saveUser("Игорь", "Игорев", (byte) 23);
         userService.saveUser("Максим", "Максимов", (byte) 24);
         userService.saveUser("Олег", "Олегов", (byte) 25);
