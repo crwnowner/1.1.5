d99656685d54b19c6267b80379a8b1357b57b176
crwnowner <8maxabb1122@mail.ru>
2023-11-22 14:41:42 +0300

    Completed 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,16 +10,16 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
-        //userService.createUsersTable();
-//        userService.saveUser("Игорь", "Игорев", (byte) 23);
-//        userService.saveUser("Максим", "Максимов", (byte) 24);
-//        userService.saveUser("Олег", "Олегов", (byte) 25);
-//        userService.saveUser("Ибрагим", "Ибрагимов", (byte) 26);
-//        for (User user : userService.getAllUsers()) {
-//            System.out.println(user.toString());
-//        }
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
-//        Util.closeConnection();
+        userService.createUsersTable();
+        userService.saveUser("Игорь", "Игорев", (byte) 23);
+        userService.saveUser("Максим", "Максимов", (byte) 24);
+        userService.saveUser("Олег", "Олегов", (byte) 25);
+        userService.saveUser("Ибрагим", "Ибрагимов", (byte) 26);
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user.toString());
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeSessionFactory();
     }
 }
